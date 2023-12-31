f371be6dbebe368c5b112d4aff9f0b07950b1dee
sehwan <nu940907@gmail.com>
2023-12-31 21:54:31 +0900

    DDD First Commit

---
+++ src/main/java/com/start/domain_driven_design/DomainDrivenDesignApplication.java
@@ -0,0 +1,13 @@
+package com.start.domain_driven_design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainDrivenDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainDrivenDesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/start/domain_driven_design/DomainDrivenDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.start.domain_driven_design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainDrivenDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
